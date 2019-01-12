82ae4ca5c2a0a4cb1a92c049677371afc8aaf3ad
thebearingedge <contact@timdav.is>
2019-01-11 21:51:03 -0800

    Create a package.json with a project name, start script. #1

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ const cwd = process.cwd()
 
 yargs
   .command(
-    'node <projectName>', 
-    'create a node project', 
-    ()=>{}, 
+    'node <projectName>',
+    'create a node project',
+    ()=>{},
     async ({projectName}) => {
       const exists = await fs.pathExists(path.join(cwd, projectName))
       if (exists) {
         console.error('Error: Project folder already exists')
         process.exit(1)
       }
-      await fs.mkdir(path.join(cwd, projectName))
+      const projectPath = path.join(cwd, projectName)
+      await fs.mkdir(projectPath)
+      const packageJson = JSON.stringify({
+        name: projectName,
+        version: "1.0.0",
+        main: "index.js",
+        scripts: {
+          start: "node ."
+        }
+      }, null, 2)
+      await fs.writeFile(path.join(projectPath, 'package.json'), packageJson)
   })
   .parse()
