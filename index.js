d754ad4f4b03d80228669444270ab7fb0b79c50a
thebearingedge <contact@timdav.is>
2019-01-11 22:04:09 -0800

    Create an index.js. #1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const fs = require('fs-extra')
 const path = require('path')
-
+const fs = require('fs-extra')
 const yargs = require('yargs')
+
 const cwd = process.cwd()
 
 yargs
   .command(
     'node <projectName>',
     'create a node project',
-    ()=>{},
-    async ({projectName}) => {
+    () => {},
+    async ({ projectName }) => {
       const exists = await fs.pathExists(path.join(cwd, projectName))
       if (exists) {
         console.error('Error: Project folder already exists')
@@ -19,12 +19,14 @@ yargs
       await fs.mkdir(projectPath)
       const packageJson = JSON.stringify({
         name: projectName,
-        version: "1.0.0",
-        main: "index.js",
+        version: '1.0.0',
+        main: 'index.js',
         scripts: {
-          start: "node ."
+          start: 'node .'
         }
       }, null, 2)
       await fs.writeFile(path.join(projectPath, 'package.json'), packageJson)
-  })
+      const indexJs = 'console.log(\'Hello, World!\')'
+      await fs.writeFile(path.join(projectPath, 'index.js'), indexJs)
+    })
   .parse()
