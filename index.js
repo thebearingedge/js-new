d09c23616a4719e7af363938b5d1cdd78094e640
MouseZero <rm@mousezero.com>
2019-01-11 22:19:07 -0800

    change index.js to prettier format #1

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const path = require('path')
-const fs = require('fs-extra')
-const yargs = require('yargs')
+const path = require("path");
+const fs = require("fs-extra");
+const yargs = require("yargs");
 
-const cwd = process.cwd()
+const cwd = process.cwd();
 
 yargs
   .command(
-    'node <projectName>',
-    'create a node project',
+    "node <projectName>",
+    "create a node project",
     () => {},
     async ({ projectName }) => {
-      const exists = await fs.pathExists(path.join(cwd, projectName))
+      const exists = await fs.pathExists(path.join(cwd, projectName));
       if (exists) {
-        console.error('Error: Project folder already exists')
-        process.exit(1)
+        console.error("Error: Project folder already exists");
+        process.exit(1);
       }
-      const projectPath = path.join(cwd, projectName)
-      await fs.mkdir(projectPath)
-      const packageJson = JSON.stringify({
-        name: projectName,
-        version: '1.0.0',
-        main: 'index.js',
-        scripts: {
-          start: 'node .'
-        }
-      }, null, 2)
-      await fs.writeFile(path.join(projectPath, 'package.json'), packageJson)
-      const indexJs = 'console.log(\'Hello, World!\')'
-      await fs.writeFile(path.join(projectPath, 'index.js'), indexJs)
-    })
-  .parse()
+      const projectPath = path.join(cwd, projectName);
+      await fs.mkdir(projectPath);
+      const packageJson = JSON.stringify(
+        {
+          name: projectName,
+          version: "1.0.0",
+          main: "index.js",
+          scripts: {
+            start: "node ."
+          }
+        },
+        null,
+        2
+      );
+      await fs.writeFile(path.join(projectPath, "package.json"), packageJson);
+      const indexJs = "console.log('Hello, World!')";
+      await fs.writeFile(path.join(projectPath, "index.js"), indexJs);
+    }
+  )
+  .parse();
